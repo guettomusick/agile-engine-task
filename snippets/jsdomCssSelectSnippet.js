e9427cfeefe89c96d20e7eab9be486397420f92a
Alexander Rusanov <alexander.rusanov@agileengine.com>
2018-02-22 01:32:11 +0200

    Added listing of attributes

--- snippets/jsdomCssSelectSnippet.js
+++ snippets/jsdomCssSelectSnippet.js
@@ -12,6 +12,8 @@ try {
   
   const button = dom.window.document.querySelector(cssQuery);
   logger.info(`Successfully found element. Element Text: ${button.textContent}`);
+  const array = Array.prototype.slice.apply(button.attributes);
+  logger.info(array.map(attr => `${attr.name} = ${attr.value}`).join(', '));
 } catch (err) {
   logger.error('Error trying to find element by css selector', err);
 }
--- snippets/jsdomFindByIdSnippet.js
+++ snippets/jsdomFindByIdSnippet.js
@@ -12,6 +12,8 @@ try {
 
   const button = dom.window.document.getElementById(targetElementId)
   logger.info(`Successfully found element. Element Text: ${button.textContent}`);
+  const array = Array.prototype.slice.apply(button.attributes);
+  logger.info(array.map(attr => `${attr.name} = ${attr.value}`).join(', '));
 } catch (err) {
   logger.error('Error trying to find element by id', err);
 }
